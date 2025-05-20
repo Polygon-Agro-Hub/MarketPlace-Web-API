627aa5a99a1c687008e5111cb31ee5d4430d6053
Pasan-FDo <pasanfernando.work@gmail.com>
2025-05-20 16:31:52 +0530

    Refactor package cart handling: update DAO methods and validation schema for improved cart management

--- validations/product-validation.js
+++ validations/product-validation.js
@@ -4,17 +4,20 @@ exports.packageDetailsSchema = Joi.object({
     packageId: Joi.number().integer().required()
 });
 
-exports.packageAddToCartSchema = Joi.array().items(
-    Joi.object({
-        id: Joi.number().integer().positive().required(),
-        packageId: Joi.number().integer().positive().required(),
-        quantity: Joi.number().required(),
-        quantityType: Joi.string().valid('Kg', 'g').required(),
-        displayName: Joi.string().required(),
-        mpItemId: Joi.number().integer().positive().required()
-    })
-).min(1).required(); // Ensures at least one item in the array
+// exports.packageAddToCartSchema = Joi.array().items(
+//     Joi.object({
+//         id: Joi.number().integer().positive().required(),
+//         packageId: Joi.number().integer().positive().required(),
+//         quantity: Joi.number().required(),
+//         quantityType: Joi.string().valid('Kg', 'g').required(),
+//         displayName: Joi.string().required(),
+//         mpItemId: Joi.number().integer().positive().required()
+//     })
+// ).min(1).required(); // Ensures at least one item in the array
 
+exports.packageAddToCartSchema = Joi.object({
+    id: Joi.number().positive().required()
+});
 
 
 exports.productDetailsSchema = Joi.object({
